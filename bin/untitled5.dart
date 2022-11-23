4d42d7ec31001f1d4eb09892ad704a5855561487
harikrishnan <kphari891@gmail.com>
2022-11-23 15:59:19 +0530

    Initial commit

---
+++ bin/untitled5.dart
@@ -0,0 +1,12 @@
+void main(){
+  var a=5;
+  var b=10;
+  print('a+b = ${a+b}');
+  print('a-b = ${a-b}');
+  print('a*b = ${a*b}');
+  print('a/b = ${a/b}');
+  print('a~/b = ${a~/b}');
+  print('a%b = ${a%b}');
+  print('-(a+b) = ${-(a+b)}');
+
+}
---
+++ test/untitled5_test.dart
@@ -0,0 +1,8 @@
+import 'package:untitled5/untitled5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
